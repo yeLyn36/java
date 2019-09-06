c496c359900631a04436203bd48ae44ec3c99ab1
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-06 09:07:45 +0900

    어쩌다 윈도우에 레이블 추가

--- 0906_GUI/src/GUI_Jframe.java
+++ 0906_GUI/src/GUI_Jframe.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class GUI_Jframe {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+
+        JLabel label = new JLabel("오늘은 집 가는 날 너무 좋아 행복해");
+        frame.add(label);
+
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
