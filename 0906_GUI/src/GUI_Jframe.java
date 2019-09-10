c615683c054b75e1042654302c2513134336172b
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-11 08:46:44 +0900

    frame, panel, label, button

--- 0906_GUI/src/GUI_Jframe.java
+++ 0906_GUI/src/GUI_Jframe.java
@@ -3,9 +3,14 @@ import javax.swing.*;
 public class GUI_Jframe {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+        JPanel panel = new JPanel();
 
         JLabel label = new JLabel("오늘은 집 가는 날 너무 좋아 행복해");
-        frame.add(label);
+        JButton button = new JButton("꼬르륵");
+
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
 
         frame.pack();
         frame.setVisible(true);
