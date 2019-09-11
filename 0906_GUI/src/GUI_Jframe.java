fdb98defe8f8fad17ba233fbb13f40aeb6b7be98
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-11 09:09:05 +0900

    Sample

--- 0906_GUI/src/GUI_Jframe.java
+++ 0906_GUI/src/GUI_Jframe.java
@@ -1,8 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI_Jframe {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+
+        Dimension dimen = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int 화w = (int)dimen.getWidth();
+        int 화h = (int)dimen.getHeight();
+        int 창w = 400;
+        int 창h = 200;
+
+        frame.setPreferredSize(new Dimension(창w, 창h));
+        frame.setLocation((화w-창w)/2, (화h-창h)/2);
+
         JPanel panel = new JPanel();
 
         JLabel label = new JLabel("오늘은 집 가는 날 너무 좋아 행복해");
---
+++ 0906_GUI/src/Sample.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
