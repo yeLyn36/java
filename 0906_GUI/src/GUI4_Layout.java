3c14383e572d600df723c5cafa3fe03fce79f792
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-10-02 08:51:47 +0900

    GridLayout, row, cols

---
+++ 0906_GUI/src/GUI4_Layout.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI4_Layout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        GridLayout gl = new GridLayout(2, 5);
+        for (int i = 0; i < 5; i++){
+            JButton btn = new JButton("button" + i);
+            panel.add(btn);
+        }
+        panel.setLayout(gl);
+
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
